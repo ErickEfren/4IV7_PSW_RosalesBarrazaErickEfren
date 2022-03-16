f9826c69c0f2b9c29ec7ba9c41cdf995550fafc7
ErickEfren <erosalesb2000@alumno.ipn.mx>
2022-03-15 19:37:59 -0600

    js

---
+++ 07Algoritmia/js/logica.js
@@ -0,0 +1,9 @@
+//problema 3
+function problema3(){
+    //definir un alfabeto
+    var alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 
+                'X', 'Y', 'Z']; 
+    //oBTENER LA CADENA DEL INPUT Y SEPARAR POR COMAS
+
+    var p3_int = document.querySelector('#p3-input').value
+}
