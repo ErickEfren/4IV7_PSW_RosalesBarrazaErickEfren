b8adcebe310e50aba0bc524da8ab6dc0f3b982ba
ErickEfren <erosalesb2000@alumno.ipn.mx>
2022-03-21 11:12:48 -0600

    js

--- 07Algoritmia/js/logica.js
+++ 07Algoritmia/js/logica.js
@@ -1,4 +1,10 @@
 //Problema 1
+function validarn(e) {
+    var teclado = (document.all) ? e.keyCode : e.which;
+    if (teclado == 8 || teclado == 32) return true;    
+    var patron = /[a b c d e f g h i j k l m n ñ o p q r s t u v w x y z A B C D E F G H I J K L M N Ñ O P Q R S T U V W X Y Z á Á é É í Í ó Ó ú Ú ü Ü]/;
+    var codigo = String.fromCharCode(teclado);
+    return patron.test(codigo)
 function reverse(palabra){
     return palabra.split("").reverse().join("");
 }
