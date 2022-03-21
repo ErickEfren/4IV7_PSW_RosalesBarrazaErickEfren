06d03358d38dc5fd38d3539511bef808889981cf
ErickEfren <erosalesb2000@alumno.ipn.mx>
2022-03-21 11:13:11 -0600

    js

--- 07Algoritmia/js/logica.js
+++ 07Algoritmia/js/logica.js
@@ -9,9 +9,6 @@ function reverse(palabra){
     return palabra.split("").reverse().join("");
 }
 function problema1(){
-    var caracteres = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 
-    'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-    'x', 'y', 'z'];
     var p1_input = document.querySelector('#p1-input').value;
     var p1_palabras = p1_input.split(' ');
     var final = "";
